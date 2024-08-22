0f6fb6df906b4a5599fb11de7ee66554a98d6c17
NpmStart2 <npmstart2@gmail.com>
2024-08-22 14:30:27 +0300

    change github link

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       {/* Home נמצא בקומפוננטת */}
       <img src={`${process.env.PUBLIC_URL}/images/logo.jpg`} className='logo' alt="logo" />
       <h2> ...האתר בשלבי פיתוח</h2>
-      <h1><a href='https://github.com/NpmStart2' target="_blank">Github</a> מפתחים? עקבו אחרינו ב </h1>
+      <h1><a href='https://github.com/NpmStart0' target="_blank">Github</a> מפתחים? עקבו אחרינו ב </h1>
       <h2 className='handwritting'><a href='mailto:npmstart2@gmail.com' target="_blank">npmstart2@gmail.com</a> ג'וניורית? רוצה להיות חלק מהצוות המוביל? צרי איתנו קשר  </h2>
     </div>
   );
