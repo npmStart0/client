3f1b0d495a64043f471a253c0aee13193a7c37b8
RutiRozenberg <ruti7633@gmail.com>
2024-09-10 13:19:53 +0300

    Arranging the AboutUs.jsx file.

--- src/project/components/AboutUs.jsx
+++ src/project/components/AboutUs.jsx
@@ -1,34 +1,35 @@
+import { Box, Container, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
 export const AboutUs = () => {
     return (
         <Box sx={{
-            backgroundColor: 'background.default', 
-            height: '100vh', 
-            // width: '100vw', 
-            display: 'flex', 
-            flexDirection: 'column', 
-            justifyContent: 'center', 
-            alignItems: 'center', 
-            padding: '20px',
-            boxSizing: 'border-box', // Make sure the margins are included in the width and height
+            backgroundColor: 'background.default',
         }}>
-             <img src={`${process.env.PUBLIC_URL}/images/logo.jpg`} alt="logo" style={{ display:'block', marginRight: 'auto', marginLeft: 'auto' }} />
-            <Typography variant="h2" style={{ fontFamily: 'zrima' }} sx={{color: 'grey.100'}}>
-                ...האתר בשלבי פיתוח
-            </Typography>
-            <Typography variant="h4" style={{ fontFamily: 'Courier Prime' }} sx={{color: 'grey.100'}}>
-                <Link href="https://github.com/NpmStart0" target="_blank" underline="hover" sx={{color: 'primary.main'}}>
-                    Github
-                </Link> 
-                מפתחים? עקבו אחרינו ב
-            </Typography>
-            <Typography variant="h5" style={{ fontFamily: 'zrima', marginTop: '20px' }} sx={{color: 'grey.100'}}>
-                <Link href="mailto:npmstart2@gmail.com" underline="hover" sx={{color: 'primary.main'}}>
-                    npmstart2@gmail.com
-                </Link> 
-                ג'ונרית? רוצה להיות חלק מהצוות המוביל? צרי איתנו קשר
-            </Typography>
+            <Container sx={{
+                height: '100vh',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}>
+                <img src={`${process.env.PUBLIC_URL}/images/logo.jpg`} alt="logo" style={{ display: 'block', marginRight: 'auto', marginLeft: 'auto' }} />
+                <Typography variant="h2" style={{ fontFamily: 'zrima' }} sx={{ color: 'grey.100' }}>
+                    ...האתר בשלבי פיתוח
+                </Typography>
+                <Typography variant="h4" style={{ fontFamily: 'Courier Prime' }} sx={{ color: 'grey.100' }}>
+                    <Link href="https://github.com/NpmStart0" target="_blank" underline="hover" sx={{ color: 'primary.main' }}>
+                        Github
+                    </Link>
+                    מפתחים? עקבו אחרינו ב
+                </Typography>
+                <Typography variant="h5" style={{ fontFamily: 'zrima', marginTop: '20px' }} sx={{ color: 'grey.100' }}>
+                    <Link href="mailto:npmstart2@gmail.com" underline="hover" sx={{ color: 'primary.main' }}>
+                        npmstart2@gmail.com
+                    </Link>
+                    ג'ונרית? רוצה להיות חלק מהצוות המוביל? צרי איתנו קשר
+                </Typography>
+            </Container>
         </Box>
     );
 };
-
-
