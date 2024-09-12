ebbb34a8c3d5428a8e12655e73293495b886ad06
RutiRozenberg <ruti7633@gmail.com>
2024-09-13 00:27:22 +0300

    Add definitions to ESLint.

--- src/project/components/Routing.jsx
+++ src/project/components/Routing.jsx
@@ -1,12 +1,10 @@
-import { Route, Routes } from "react-router"
-import { Home } from "./Home.jsx"
-import { AboutUs } from "./AboutUs.jsx"
+import { Route, Routes } from 'react-router';
+import { AboutUs } from './AboutUs.jsx';
 
 export const Routing = () => {
     return <>
         <Routes>
-            <Route path="/Home" element={<Home></Home>}></Route>
             <Route path="/" element={<AboutUs></AboutUs>}></Route>
         </Routes>
-    </>
-}
+    </>;
+};
