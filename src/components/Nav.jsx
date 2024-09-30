87a7e1b28721534e729d2ac408b43c11e1f1b56c
dini_landy <dini0527197044@gmail.com>
2024-09-30 14:44:40 +0300

    create nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,39 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
+
+const pages = [   
+    { name: 'אודותינו', route: '/about' },
+    { name: 'דיונים', route: '/discuss' },
+    { name: 'בית', route: '/home' },
+
+];
+
 export const Nav = () => {
     return <>
-
+        <AppBar position="static" sx={{ backgroundColor: 'background.default', boxShadow: 'none' }}>
+            <Toolbar sx={{ borderBottom: '1px solid lightgray' ,justifyContent: 'flex-end'}} >
+                {pages.map((page) => (
+                    <Button
+                        key={page.name}
+                        color="inherit"
+                        component={Link}
+                        to={page.route}>
+                        {page.name}
+                    </Button>
+                ))}
+                <img
+                    src={`${process.env.PUBLIC_URL}/images/logo.jpg`}
+                    alt='logo'
+                    style={{ 
+                        display: 'block', 
+                        width:'100px'
+                    }}
+                />
+            </Toolbar>
+        </AppBar>
     </>;
 };
+
--- src/components/Routing.jsx
+++ src/components/Routing.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router';
 import { AboutUs } from './AboutUs.jsx';
+import { Home } from './Home.jsx';
 
 export const Routing = () => {
     return <>
         <Routes>
-            <Route path='/' element={<AboutUs></AboutUs>}></Route>
+            <Route path='/about' element={<AboutUs></AboutUs>}></Route>
+            <Route path='/home' element={<Home></Home>}></Route>
         </Routes>
     </>;
 };
