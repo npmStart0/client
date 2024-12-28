efa7318ddf74d80dd8608d3f6b9cd135259cc3c1
RutiRozenberg <ruti7633@gmail.com>
2024-12-28 23:41:42 +0200

    Improve API interaction functions.

--- src/utils/api/api.js
+++ src/utils/api/api.js
@@ -1,49 +1,19 @@
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_API_URL;
-
-const apiService = axios.create({
-    baseURL,
-});
-
-const getData = async (path) => {
-    try {
-        const response = await apiService.get(path);
+const sendRequest = async (endpoint, method, data= null, token=null,  baseURL = process.env.REACT_APP_API_URL) => {
+    const url = `${baseURL}${endpoint}`;
+    const headers = token ? { Authorization: `Bearer ${token}` } : {};
+    try{
+        const response = await axios.request({
+            url,
+            method,
+            headers,            
+            data,
+        });
         return response.data;
-    } catch (error) {
-        console.error(`Error fetching data from ${path}:`, error);
-        return null;
+    } catch(error){
+        console.error('Request faild ,' ,error);
     }
 };
 
-const postData = async (path, data) => {
-    try {
-        const response = await apiService.post(path, data);
-        return response.data;
-    } catch (error) {
-        console.error(`Error posting data to ${path}:`, error);
-        return null;
-    }
-};
-
-const updateData = async (path, data) => {
-    try {
-        const response = await apiService.put(path, data);
-        return response.data;
-    } catch (error) {
-        console.error(`Error updating data at ${path}:`, error);
-        return null;
-    }
-};
-
-const deleteData = async (path) => {
-    try {
-        const response = await apiService.delete(path);
-        return response.data;
-    } catch (error) {
-        console.error(`Error deleting data at ${path}:`, error);
-        return null;
-    }
-};
-
-export { getData , postData , updateData , deleteData };
+export default sendRequest;
